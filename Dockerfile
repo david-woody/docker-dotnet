435c215a04a0761119cb42df2699e95699880ac0
Wallent <toigkissdxw@163.com>
2018-07-25 15:38:45 +0800

    modify

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM microsoft/dotnet:sdk 
+RUN apt-get update && apt-get install -y \
+    libunwind8
 COPY start.sh /run
 WORKDIR /code
 CMD ["../run/start.sh"]
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /code/mp_demox/
-
-dotnet run
+cd /code/
+dotnet publish -r linux-x64 -c release
+dotnet mp_demox/bin/Release/netcoreapp2.0/linux-x64/publish/mp.marketing.api.dll
