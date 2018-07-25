7f608ce766cfb38b2c6cb769af6ee469a513d1d3
Wallent <toigkissdxw@163.com>
2018-07-25 21:26:00 +0800

    modify

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /code/
-dotnet publish -r linux-x64 -c release
+dotnet publish --framework  netcoreapp2.0 --runtime linux-x64 -c release
 dotnet mp_demox/bin/Release/netcoreapp2.0/linux-x64/publish/mp.marketing.api.dll
