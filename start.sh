0a02632f4c7eaf6f680245ee6436376f7733870d
Wallent <toigkissdxw@163.com>
2018-07-26 16:21:40 +0800

    modify

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 cd /code/
 sed -i 's/47.96.152.89/postgresql/g' mp_demox/appsettings.json
 dotnet publish --framework  netcoreapp2.0 --runtime linux-x64 -c release
-dotnet bin/Release/netcoreapp2.0/linux-x64/publish/mp.marketing.api.dll
+dotnet mp_demox/bin/Release/netcoreapp2.0/linux-x64/publish/mp.marketing.api.dll
